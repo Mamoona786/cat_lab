59a793d309ee28b8896f4137d835326450a3464b
Mamoona <mamoonaghania786@gmail.com>
2023-07-14 02:10:01 +0500

    Added cat likes function

---
+++ cats.js
@@ -0,0 +1,11 @@
+// this program will tel us about our cats
+let cats = [
+    {name: "Tabby", likes: "catnip"},
+    {name: "Bob Ross", likes: "paint"},
+    {name: "Chaos", likes: "knocking over plants"}];
+
+for (let i = 0 ; i < cats.length ; i++)
+{
+    // we will use string interpolation
+    console.log(`${cats[i].name} likes ${cats[i].likes}`);
+}
